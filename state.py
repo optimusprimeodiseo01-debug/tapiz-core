468d5784c5d47df60df192006023baaf628a54f0
Ivan <optimusprimeodiseo01@gmail.com>
2026-04-29 17:19:06 -0300

    tapiz sync | s=1.0000 | K=1.000

---
+++ state.py
@@ -0,0 +1,14 @@
+import json
+
+STATE_FILE = "estado.json"
+
+def load_state():
+    try:
+        with open(STATE_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save_state(state):
+    with open(STATE_FILE, "w") as f:
+        json.dump(state, f, indent=2)
